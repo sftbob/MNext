01676c95bf6d8b712558be1cff34a8b63ceac87c
sftbob <111048941+sftbob@users.noreply.github.com>
2024-11-11 10:05:43 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -337,17 +337,17 @@ const CONFIG = {
 
   // 页面底部的行动呼吁模块
   STARTER_CTA_ENABLE: true,
-  STARTER_CTA_TITLE: '你还在等待什么呢？',
-  STARTER_CTA_TITLE_2: '现在开始吧',
+  STARTER_CTA_TITLE: '立刻預約房仲哖大師',
+  STARTER_CTA_TITLE_2: '您的委託是我的責任',
   STARTER_CTA_DESCRIOTN:
-    '访问NotionNext的操作文档，我们提供了详细的教程，帮助你即刻搭建站点',
+    '一杯咖啡的時間，得到重要的觀念，避免房屋買賣的糾紛，獲得合理的銷售結果，這樣的好事您還等甚麼?',
   STARTER_CTA_BUTTON: true, // 是否显示按钮
   STARTER_CTA_BUTTON_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
-  STARTER_CTA_BUTTON_TEXT: '开始体验',
+    'https://forms.gle/qzc2RjcazRtwPeoy5',
+  STARTER_CTA_BUTTON_TEXT: '點我預約',
 
   STARTER_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
-  STARTER_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // 重定向域名
+  STARTER_POST_REDIRECT_URL: 'https://master.ponien.com', // 重定向域名
   STARTER_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_STARTER_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
 export default CONFIG

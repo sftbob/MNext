f708bbd5a678e6d260724e3cd92bc55b7746add1
sftbob <111048941+sftbob@users.noreply.github.com>
2024-11-11 16:34:26 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -46,18 +46,15 @@ const CONFIG = {
   STARTER_FEATURE_2_TITLE_1: '案件租賃', // 特性2
   STARTER_FEATURE_2_TEXT_1: '租屋大小事，交給我們！ 不論是房東想出租房屋，還是租客想找房子，我們都能為您提供專業的租賃服務，讓您省心又省力。', // 特性2
   STARTER_FEATURE_2_BUTTON_TEXT: '', // 特性2
-  STARTER_FEATURE_2_BUTTON_URL:
-    '', // 特性2
+  STARTER_FEATURE_2_BUTTON_URL: '', // 特性2
 
   STARTER_FEATURE_3_TITLE_1: '投資法規', // 特性3
   STARTER_FEATURE_3_TEXT_1: '房地產交易，交給專家最安心！ 我們提供專業的法律與稅務諮詢，協助您順利完成國內外房地產交易。從市場分析、投資規劃到法律文件處理，我們全程為您把關，讓您無後顧之憂。', // 特性3
-  STARTER_FEATURE_3_BUTTON_TEXT: '法規資料', // 特性3
+  STARTER_FEATURE_3_BUTTON_TEXT: '', // 特性3
   STARTER_FEATURE_3_BUTTON_URL: '', // 特性3
 
   STARTER_FEATURE_4_TITLE_1: '加入大師', // 特性4
-  STARTER_FEATURE_4_TEXT_1: '想挑戰高薪？ 加入我們，成為頂尖豪宅銷售高手！
-新鮮人不怕沒經驗！ 我們提供完善的培訓，讓你快速上手。
-熱愛房地產，想與一群志同道合的人一起打拼嗎？ 歡迎加入我們的大家庭！', // 特性4
+  STARTER_FEATURE_4_TEXT_1: '想挑戰高薪？ 加入我們，成為頂尖豪宅銷售高手！新鮮人不怕沒經驗！ 我們提供完善的培訓，讓你快速上手。熱愛房地產，想與一群志同道合的人一起打拼嗎？ 歡迎加入我們的大家庭！', // 特性4
   STARTER_FEATURE_4_BUTTON_TEXT: '大師專線', // 特性4
   STARTER_FEATURE_4_BUTTON_URL: '', // 特性4
 
